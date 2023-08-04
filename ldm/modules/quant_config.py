d282db02952d071df5ed1e136ff6308a37e4ebb3
Chuiyu Wang <aiyinxitan@126.com>
2023-08-04 18:57:44 +0100

    Updated gitignore

--- ldm/modules/quant_config.py
+++ ldm/modules/quant_config.py
@@ -25,60 +25,60 @@ class UNetQuantConfig():
             "unet_out",
             "unet_codebook",
             ]
-        self.quant_config["attpool_qkf"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["attpool_c"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["upsample"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["downsample"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+        self.quant_config["attpool_qkf"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["attpool_c"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["upsample"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["downsample"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         self.quant_config["avg_pool2d"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
         self.quant_config["silu_res_in"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
-        self.quant_config["resblock_in"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
+        self.quant_config["resblock_in"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         self.quant_config["silu_res_emb"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
-        self.quant_config["resblock_emb"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
+        self.quant_config["resblock_emb"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         self.quant_config["silu_res_out"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
-        self.quant_config["resblock_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["resblock_skip"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["attblock_qkv"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["attblock_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["unet_temb"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
+        self.quant_config["resblock_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["resblock_skip"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["attblock_qkv"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["attblock_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["unet_temb"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         self.quant_config["silu_unet_temb"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
-        self.quant_config["unet_in"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
+        self.quant_config["unet_in"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         self.quant_config["silu_unet_out"] = {"name": "integer",
-                                           "data_in_width": 8, "data_in_frac_width": 4}
-        self.quant_config["unet_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
-        self.quant_config["unet_codebook"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 4,
-                                            "data_in_width": 8, "data_in_frac_width": 4, 
-                                            "bias_width": 8, "bias_frac_width": 4}
+                                           "data_in_width": 8, "data_in_frac_width": 5}
+        self.quant_config["unet_out"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
+        self.quant_config["unet_codebook"] = {"name": "integer", "weight_width": 8, "weight_frac_width": 5,
+                                            "data_in_width": 8, "data_in_frac_width": 5, 
+                                            "bias_width": 8, "bias_frac_width": 5}
         print("Loaded quantization config.")
     
     def get(self, item: str) -> Dict:
